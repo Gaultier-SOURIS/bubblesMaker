fbb1a0c864d8ac741f70316e5150decc5cf46a88
Gaultier SOURIS <gaultier.souris@gmail.com>
2024-02-23 23:08:31 +0100

    des bubulles, des bubulles

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Bubbles from '../Bubbles/Bubbles';
+
+function App() {
+  return (
+    <div className="container">
+      <Bubbles />
+    </div>
+  );
+}
+
+export default App;
